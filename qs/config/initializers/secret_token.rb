8f2cda9d7321b2108183092842a877552fc6b221
Paul Rosenzweig <PaulARosenzweig@gmail.com>
2012-04-21 13:54:11 -0400

    created rails app

---
+++ qs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qs::Application.config.secret_token = '070ab01582b5f99e690e34e369356304ae05b9815dc6c154416b051c3a960ee7efae6fbddcd7b41be48edeb9be2c01be412d87939a847ebbfdb42a11b1346cb3'
