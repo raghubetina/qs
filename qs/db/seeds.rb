66da064ba297ae2500b10fdafd00da131236b26c
Paul Rosenzweig <PaulARosenzweig@gmail.com>
2012-04-22 18:20:39 -0400

    modified seeds and fixed timing for playback

--- qs/db/seeds.rb
+++ qs/db/seeds.rb
@@ -9,33 +9,34 @@
 Lesson.destroy_all
 Question.destroy_all
 Vote.destroy_all
+Visit.destroy_all
 
-lessons = %w(uchack painting101 mcatochem jqueryintro sat2math)
+# lessons = %w(uchack painting101 mcatochem jqueryintro sat2math)
 
-lessons.each do |lesson|
-  Lesson.create name: lesson,
-    embed_code: "10938654",
-    notes: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-end
+# lessons.each do |lesson|
+#   Lesson.create name: lesson,
+#     embed_code: "10938654",
+#     notes: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+# end
 
-questions = [
-  "What are the judging criteria?",
-  "When does it start?",
-  "Where is it?",
-  "How many teams have signed up?",
-  "Vim or TextMate or emacs?",
-  "Coke or Pepsi?",
-  "iPhone or Android?",
-  "Mac or Windows?",
-  "What are the prizes?"
-  ]
+# questions = [
+#   "What are the judging criteria?",
+#   "When does it start?",
+#   "Where is it?",
+#   "How many teams have signed up?",
+#   "Vim or TextMate or emacs?",
+#   "Coke or Pepsi?",
+#   "iPhone or Android?",
+#   "Mac or Windows?",
+#   "What are the prizes?"
+#   ]
 
-Lesson.all.each do |lesson|  
-  questions.each do |question|
-    q = lesson.questions.create content: question
-    rand(20).times do |u|
-      q.votes.create user_id: u
-    end
-  end
-end
+# Lesson.all.each do |lesson|
+#   questions.each do |question|
+#     q = lesson.questions.create content: question
+#     rand(20).times do |u|
+#       q.votes.create user_id: u
+#     end
+#   end
+# end
 
